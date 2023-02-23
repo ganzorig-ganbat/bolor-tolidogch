1a59553fd190c05d540f5b858c981f5ca5173056
Kenzo <swganzo@gmail.com>
2023-02-23 15:13:38 -0500

    Fixes contextmenu functionality

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,22 @@
-// Watch for changes to the user's options & apply them
-chrome.storage.onChanged.addListener((changes, area) => {
-  if (area === "sync" && changes.options?.newValue) {
-    const debugMode = Boolean(changes.options.newValue.debug);
-    console.log("enable debug mode?", debugMode);
-    setDebugMode(debugMode);
+/* Copyright (c) 2017 SWGANZO. All rights reserved. */
+
+import { bolorLocales } from "./locales.js";
+import { updateWindow } from "./window.js";
+
+chrome.runtime.onInstalled.addListener(async () => {
+  for (const { direction, title } of bolorLocales) {
+    chrome.contextMenus.create({
+      id: direction,
+      title: title,
+      type: "normal",
+      contexts: ["selection"],
+    });
   }
+});
+
+chrome.contextMenus.onClicked.addListener((item) => {
+  const direction = item.menuItemId;
+  const search_text = item.selectionText;
+  console.log(direction, search_text);
+  updateWindow(search_text, direction);
 });
